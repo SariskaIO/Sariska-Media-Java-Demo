d5f71aa04e791aee401657cec692a509d8baff36
dipak140 <dipak.sisodiya@trilogy.com>
2022-12-31 15:30:12 +0530

    Update MainActivity.java

--- app/src/main/java/io/sariska/sariska_media_java_demo/MainActivity.java
+++ app/src/main/java/io/sariska/sariska_media_java_demo/MainActivity.java
@@ -3,6 +3,7 @@ package io.sariska.sariska_media_java_demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import io.sariska.sdk.SariskaMediaTransport;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SariskaMediaTransport.initializeSdk(getApplication());
     }
 }
