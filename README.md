d1e8869745a94684a6d4eb49fa4f4afa37ad5bc2
dipak140 <dipak.sisodiya@trilogy.com>
2022-12-31 16:48:12 +0530

    Working Solution

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Sariska-Media-Java-Demo
+
+
+Sariska Media provides powerful Java API's for developing real-time applications.
+
+This is a sample app to  showcase basic functionality of Sariska Media API's which can be leveraged for your Native Android development.
+
+
+
+The sample app lets you create a room with a name of your choice. Once the user clicks on the start meeting button, a conference is created and any other user with the same room name can join in.
+
+
+https://user-images.githubusercontent.com/22401307/194180222-04a18d6b-85a1-4cdf-831d-6feede2eb09b.mp4
---
+++ app/src/main/java/io/sariska/sariska_media_java_demo/GetToken.java
@@ -0,0 +1,56 @@
+package io.sariska.sariska_media_java_demo;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class GetToken {
+    protected static String generateToken(String userID) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        String url = "https://api.sariska.io/api/v1/misc/generate-token";
+        String json = "{\n" +
+                "    \"apiKey\": \"249202aabed00b41363794b526eee6927bd35cbc9bac36cd3edcaa\",\n" +
+                "    \"user\": {\n" +
+                "        \"id\": \"dsds\",\n" +
+                "        \"name\": \"SomeOne\",\n" +
+                "        \"moderator\": false,\n" +
+                "        \"email\": \"dipak@work.com\",\n" +
+                "        \"avatar\":\"null\"\n" +
+                "    }\n" +
+                "}";
+        Log.d("Generated Token", "generateToken: ");
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .addHeader("Content-Type", "application/json")
+                .build();
+        try(Response response = client.newCall(request).execute()){
+            String responseString = response.body().string();
+            responseString = "[" + responseString + "]";
+            JSONArray array = new JSONArray(responseString);
+            String finalResponse = null;
+            for(int i=0; i < array.length(); i++) {
+                JSONObject object = array.getJSONObject(i);
+                finalResponse = object.getString("token");
+            }
+            return finalResponse;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            System.out.println("This cannot be done");
+            return null;
+        }
+    }
+}
+
